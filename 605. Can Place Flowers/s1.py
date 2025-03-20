9cdbbda0fcd9f22f7ce9e6492ecccca61af40d23
HEMANTH <hemanthvhb@gmail.com>
2025-03-20 08:32:51 +0530

    Update s1.py

--- 605. Can Place Flowers/s1.py
+++ 605. Can Place Flowers/s1.py
@@ -1,11 +1,11 @@
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
         for i in range(len(flowerbed)):
-            if flowerbed[i]==0:
-                prev_e=(i==0 or flowerbed[i-1]==0)
-                next_e=( i==len(flowerbed)-1 or flowerbed[i+1]==0 )
-                if next_e and prev_e :
-                    flowerbed[i]=1
-                    n-=1
- 
-        return n<=0
+            if (flowerbed[i] == 0 and 
+                (i == 0 or flowerbed[i - 1] == 0) and 
+                (i == len(flowerbed) - 1 or flowerbed[i + 1] == 0)):
+                flowerbed[i] = 1
+                n -= 1
+                if n <= 0:
+                    return True
+        return n <= 0
